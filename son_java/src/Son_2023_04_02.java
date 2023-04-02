8f700e50f69350c3fa02ce1b7a452fd8111ea2e5
sonyssh4942 <sonyssh4942@gmail.com>
2023-04-02 18:57:09 +0900

    2023-04-02 세팅공부 완료

--- son_java/src/Son_2023_04_02.java
+++
@@ -1,5 +0,0 @@
-public class Son_2023_04_02 {
-    public static void main(String[] args){
-        System.out.println("감자 테스트");
-    }
-}
---
+++ son_java/src/son_2023_04_02/test.java
@@ -0,0 +1,7 @@
+package son_2023_04_02;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("제발 되라 ");
+    }
+}
